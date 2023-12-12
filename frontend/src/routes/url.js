75d6e0a1169ae5a06f9b791aab29a69548779fe9
lihin <nlihin@gmail.com>
2023-12-12 12:52:15 +0200

    frontend build -4th try

--- frontend/src/routes/url.js
+++ frontend/src/routes/url.js
@@ -1,2 +1,2 @@
-// export const BaseURL = "https://rate2rank-0d561bf6674a.herokuapp.com/";
-export const BaseURL = "http://127.0.0.1:5000/";
+export const BaseURL = "https://rate2rank-0d561bf6674a.herokuapp.com/";
+//export const BaseURL = "http://127.0.0.1:5000/";
