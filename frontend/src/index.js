6690b0886ddf69dbb3737b14190d61d5975608ac
lihin <nlihin@gmail.com>
2024-01-11 17:58:57 +0200

    undid index.js change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,30 @@
-// import ReactDOM from "react-dom/client";
-// import { Provider } from "react-redux";
-// import App from "./App";
-// import store from "./store";
-//
-// import "./index.css";
-//
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store";
 
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import store from './store';
+import "./index.css";
 
-import './index.css';
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
+    <App />
+  </Provider>
 );
+
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import App from './App';
+// import store from './store';
+//
+// import './index.css';
+//
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router>
+//       <App />
+//     </Router>
+//   </Provider>,
+//   document.getElementById('root')
+// );
