a1742baba28b94698f9754c9c78c8fb5320cce77
lihin <nlihin@gmail.com>
2024-01-12 13:51:56 +0200

    code clean after refresh fix

--- app/routes_group.py
+++ app/routes_group.py
@@ -1,27 +1,18 @@
-from flask import Blueprint, request, jsonify
-from app.models import Group, Rate, db, Question
+from flask import Blueprint, jsonify
+from app.models import Group, Rate
 from flask_jwt_extended import current_user, jwt_required
 
 group = Blueprint('group', __name__)
 
 
-
-group = Blueprint('group', __name__)
-
-#user sends group info , i send back group_name, and q if relevent
 @group.route('/group', methods=['GET'])
 @jwt_required()
 def get_rating_info():
 
-   # group_nums = [group.number for group in Group.query.all()]
-    group_nums = [group.number for group in Group.query.filter_by(class_code = current_user.class_code)]
-    #rated_groups = [rate.group_number for rate in Rate.query.filter_by(username=current_user.username).all()]
-    #Lihi changed to accomadate exp_code
-    rated_groups = [rate.group_number for rate in
-                    Rate.query.filter_by(username=current_user.username).all()]
+    group_nums = [group.number for group in Group.query.filter_by(class_code=current_user.class_code)]
+    rated_groups = [rate.group_number for rate in Rate.query.filter_by(username=current_user.username).all()]
 
     group_nums.sort()
-    group_info = {group:(True if group in rated_groups else False) for group in group_nums}
+    group_info = {group: (True if group in rated_groups else False) for group in group_nums}
 
-    return jsonify(status = 200,data=group_info)
-
+    return jsonify(status=200, data=group_info)
--- app/routes_rate.py
+++ app/routes_rate.py
@@ -2,27 +2,10 @@ from flask import Blueprint, request, jsonify
 from app import db
 from app.models import Group, Rate, Question, CrowdRating, Rank, QuestionAnswer
 from flask_jwt_extended import current_user, jwt_required
-
+from ast import literal_eval
 from datetime import datetime
 
 rate = Blueprint('rate', __name__)
-
-
-#lihi
-@rate.route('/groups/<int:group_number>')
-@jwt_required()
-def get_group(group_number):
-    print(request.url)
-    print(request.args.get('group_number'))
-    group_name = db.session.query(Group).filter_by(number=group_number).one_or_none()
-    # if group valid
-    if group_name:
-        questions = {q.number: q.description for q in Question.query.all()}
-
-        return jsonify(status=200, data={"group_name": group_name.name, "questions": questions})
-
-    # check response number
-    return jsonify(status=400, msg="Group not registered")
 
 
 @rate.route('/rate', methods=['GET'])
@@ -73,6 +56,8 @@ def rate_page():
                                group_number=int(group_number))
         db.session.add(q_add)
 
+
+
     db.session.add(rate)
     db.session.add(crowd_ratings)
     db.session.commit()
@@ -81,37 +66,27 @@ def rate_page():
     # ToDo add check if same rates
     exs_rank = Rank.query.filter_by(username=current_user.username, date=datetime.today().date()).first()
 
-      # if users first input insert ranking to db
+    # if users first input insert ranking to db
     if not exs_rank:
         t = [(group_number, rate.rate)]
         rank = Rank(username=current_user.username, date=datetime.today().date(), list_rank=repr(t))
         db.session.add(rank)
         db.session.commit()
         return jsonify(status=200, ranking=False)
-    #check for conflicts.
-    from ast import literal_eval
+
+    # check for conflicts.
     list_rank = literal_eval(exs_rank.list_rank)
-    #eli
     for rank in list_rank:
         if rank[1] == data['rate']:
-            # db.session.commit()
-            # #this calls the pairwise questions page
-            # #that page calls routes_rank.py
-            # #we want to return here afterwards, to do the commit after this (instead of before)
             return jsonify(status=200, ranking=True, data={"rank_list": exs_rank.list_rank})
-    # eli
-    #for rank in list_rank:
-    #    if rank[1] == data['rate']:
-    #        return jsonify(status=200, ranking=True, data={"rank_list": exs_rank.list_rank})
-    #        db.session.commit()
-    #        return jsonify(status=200, ranking=False)
+
     flag = False  # check if inserted to list
     copy_list_rank = list_rank.copy()
     for index, elem in enumerate(list_rank):
         print(elem[1], data['rate'])
         if int(elem[1]) < data['rate']:
-            copy_list_rank.insert(index, ((int(group_number), data['rate'])))
-            flag=True
+            copy_list_rank.insert(index, (int(group_number), data['rate']))
+            flag = True
             break
     if not flag:
         copy_list_rank.append((int(group_number), data['rate']))
@@ -119,6 +94,3 @@ def rate_page():
     exs_rank.list_rank = repr(copy_list_rank)
     db.session.commit()
     return jsonify(status=200, ranking=False)
-
-
-
