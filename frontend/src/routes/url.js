71fc96ca00056e925c2bd04d2de354d7676e064a
lihin <nlihin@gmail.com>
2026-01-26 15:56:23 +0200

    Fix stray character in frontend url.js

--- frontend/src/routes/url.js
+++ frontend/src/routes/url.js
@@ -1,5 +1,5 @@
 //changed 25.1.2026
-export const BaseURL = (process.env.REACT_APP_API_BASE_URL || "").replace(/\/?$/, "/");q
+export const BaseURL = (process.env.REACT_APP_API_BASE_URL || "").replace(/\/?$/, "/");
 
 //export const BaseURL = "https://rate2rank-0d561bf6674a.herokuapp.com/";
 //export const BaseURL = "";
